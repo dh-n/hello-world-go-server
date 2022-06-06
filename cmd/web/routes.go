c257c0e0f68aa5a2f4fe2af9a313d3cda8fce67b
Dhanush <dhanushns@Dhanushs-Air.bbrouter>
2022-06-06 22:38:51 +0530

    replace pat router with chi router and add middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,20 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
 	"github.com/dh-n/go-course/pkg/config"
 	"github.com/dh-n/go-course/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler{
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(WriteToConsole)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dh-n/go-course
 
 go 1.18
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/justinas/nosurf v1.1.1
+)
